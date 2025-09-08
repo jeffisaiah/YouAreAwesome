120cb442f47a4540d47a65a618fed9e2d569197d
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-08 00:01:18 -0400

    Edits after Ch. 1.10 of Prof. G. SwiftUI course

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -15,24 +15,27 @@ struct ContentView: View {
     var body: some View {
         
         VStack {
-            Spacer()
-            
-            Image(imageName)
-                .resizable()
-                .scaledToFit()
-                .clipShape(RoundedRectangle(cornerRadius: 30))
-                .shadow(radius: 30)
             
             Text(message)
                 .font(.largeTitle)
                 .fontWeight(.heavy)
                 .foregroundStyle(.red)
                 .multilineTextAlignment(.center)
+                .frame(height: 100)
+                .minimumScaleFactor(0.5)
+                .animation(.easeInOut(duration:0.15), value: message)
+            Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+                .animation(.default, value: imageName)
+            
             
             Spacer()
             
                 Button("Show Message") {
-                    let messages: [String] = ["You are Awesome!", "You are Great!", "You are Cool!", "You are the very best!"]
+                    let messages: [String] = ["You are Awesome!","You will pass all your classes, and receive all A's this semester!", "You are Great!", "You are Cool!", "You are the very best!"]
                     
                     message = messages[messageNumber]
                     messageNumber += 1
