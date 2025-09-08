49666c7929eab593eb7404710119e72242e085eb
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-08 01:40:46 -0400

    Edits after Ch. 1.12 of Prof. G.

--- YouAreAwesome/ContentView.swift
+++ YouAreAwesome/ContentView.swift
@@ -10,8 +10,8 @@ import SwiftUI
 struct ContentView: View {
     @State private var message = ""
     @State private var imageName = ""
-    @State private var imageNumber = 0
-    @State private var messageNumber = 0
+    @State private var lastMessageNumber = 0
+    @State private var lastImageNumber = 0
     var body: some View {
         
         VStack {
@@ -35,14 +35,25 @@ struct ContentView: View {
             Spacer()
             
                 Button("Show Message") {
-                    let messages: [String] = ["You are Awesome!","You will pass all your classes, and receive all A's this semester!", "You are Great!", "You are Cool!", "You are the very best!"]
+                    let messages: [String] = ["You are Awesome!",
+                                              "You will pass all your classes,and receive all A's this semester!",
+                                              "You are Great!",
+                                              "You are Cool!",
+                                              "You are the very best!"]
                     
+                    var messageNumber = Int.random(in: 0...messages.count-1)
+                    while messageNumber == lastMessageNumber {
+                        messageNumber = Int.random(in: 0...messages.count-1)
+                    }
                     message = messages[messageNumber]
-                    messageNumber = Int.random(in: 0...messages.count-1)
+                    lastMessageNumber = messageNumber
                     
-                    
-                    imageName = "image\(imageNumber)"
-                    imageNumber = Int.random(in: 0...9)
+                    var imageNumber = Int.random(in: 0...9)
+                    while imageNumber == lastImageNumber {
+                        imageNumber = Int.random(in: 0...9)
+                    }
+                    imageName = "image\(Int.random(in: 0...9))"
+                    lastImageNumber = imageNumber
                 }
             .buttonStyle(.borderedProminent)
             .font(.title2)
